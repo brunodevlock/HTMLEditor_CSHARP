09bbd83868e963a1c660286dcf9b9022ea686324
Bruno André Scatena <bscatena@bestprojects.com.br>
2021-02-15 12:36:47 -0300

    HandleMenuOption

--- Menu.cs
+++ Menu.cs
@@ -15,6 +15,8 @@ namespace EditorHtml{
 
             var option = short.Parse(Console.ReadLine());
 
+            HandleMenuOption(option);
+
         }
 
         public static void DrawScreen()
@@ -57,5 +59,18 @@ namespace EditorHtml{
 
         }
 
+        public static void HandleMenuOption(short option)
+        {
+            switch(option){
+                case 1: Editor.Show(); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0: {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: Show(); break;
+            }
+        }
     }
 }
